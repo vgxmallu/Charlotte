8c03db864af1a2da75bc64df731526bec8f90fe7
Jelly <jellylopster@gmail.com>
2025-01-28 23:39:59 +0100

    TTSAVE

--- services/tiktok.py
+++ services/tiktok.py
@@ -3,12 +3,13 @@ from .base_service import BaseService
 import re
 import aiohttp
 import os
+from ttsave_api import TTSave, ContentType
 
 class TikTokService(BaseService):
     name = "Tiktok"
     def __init__(self, output_path: str = "other/downloadsTemp") -> None:
         self.output_path = output_path
-        self.BASE_URL = "http://45.13.225.104:4347/api/v2"
+        self.ttsave_client = TTSave()
 
     def is_supported(self, url: str) -> bool:
         return bool(re.match(r'https?://(?:www\.)?(?:tiktok\.com/.*|(vm|vt)\.tiktok\.com/.+)', url))
@@ -19,46 +20,25 @@ class TikTokService(BaseService):
     async def download(self, url: str) -> list:
         result = []
         try:
-            tiktok_info = await self._get_tiktok_info(url)
+            saved_files = self.ttsave_client.download(
+                url=url,
+                content_type=ContentType.Original,
+                downloads_dir=self.output_path
+            )
 
-            if tiktok_info.get("message") and tiktok_info.get("detail"):
-                raise Exception(tiktok_info)
+            if saved_files is None:
+                raise ValueError("Failed to download TikTok content")
 
-            for file in tiktok_info["files"]:
-                stream_url = file["url"]
-
-                # Генерируем имя файла из ссылки
-                filename = os.path.join(self.output_path, os.path.basename(stream_url))
-
-                # Загружаем файл по ссылке
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(f"{self.BASE_URL}{stream_url}") as response:
-                        if response.status == 200:
-                            with open(filename, "wb") as f:
-                                while chunk := await response.content.read(8192):
-                                    f.write(chunk)
-                if filename.endswith(".mp4"):
-                    result.append({"type": "video", "path": filename})
-                elif filename.endswith(".jpg") or filename.endswith(".png"):
-                    result.append({"type": "image", "path": filename})
-                elif filename.endswith(".mp3"):
-                    result.append({"type": "audio", "path": filename, "cover":  None})
+            for file in saved_files:
+                if file.endswith(".mp4"):
+                    result.append({"type": "video", "path": file})
+                elif file.endswith(".jpg") or file.endswith(".png"):
+                    result.append({"type": "image", "path": file})
+                elif file.endswith(".mp3"):
+                    result.append({"type": "audio", "path": file, "cover":  None})
 
             return result
 
         except Exception as e:
             logging.error(f"Error downloading TikTok: {str(e)}")
             return result
-
-
-    async def _get_tiktok_info(self, tiktok_url: str) -> dict:
-        url = f"{self.BASE_URL}/download"
-
-        payload = {
-            "url": tiktok_url,
-            "download_content_type": "ORIGINAL"
-        }
-
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, json=payload) as response:
-                return await response.json()
