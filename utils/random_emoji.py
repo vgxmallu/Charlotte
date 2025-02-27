8ee831f743e250aea294e76d487dc4bf0169d40f
Jelly <jellylopster@gmail.com>
2025-02-27 20:38:18 +0100

    Update random_emoji.py

--- utils/random_emoji.py
+++ utils/random_emoji.py
@@ -8,4 +8,4 @@ def random_emoji():
 
 def random_cookie_file():
     cookie_files = os.listdir("cookies")
-    return random.choice(cookie_files) if cookie_files else None
+    return f"cookies/{random.choice(cookie_files)}" if cookie_files else None
