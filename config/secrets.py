4c3288e96e56570d9bbbdc5b14e2113130b1dc74
vgxmallu <DeltaAlgor@outlook.com>
2025-08-19 00:30:08 +0530

    Update secrets.py

--- config/secrets.py
+++ config/secrets.py
@@ -3,9 +3,9 @@ import os
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("6299938157:AAFrf6Vn5QehHA3Xv3EWq3xSDBW2aws_0nE")
 
-SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
-SPOTIFY_SECRET = os.getenv("SPOTIFY_SECRET")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+SPOTIFY_CLIENT_ID = os.getenv("9bc099cf511348748c885636a99e8214")
+SPOTIFY_SECRET = os.getenv("1a49370594b543ba915a7bb8f55ec68d")
+ADMIN_ID = int(os.getenv("784589736"))
 APPLEMUSIC_DEV_TOKEN = os.getenv("APPLEMUSIC_DEV_TOKEN")
