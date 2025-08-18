78193670d901864b12718bbae58d6911bf4fbc5c
vgxmallu <DeltaAlgor@outlook.com>
2025-08-19 00:31:10 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 
-LOG_DIR = os.getenv("LOG_DIR")
+LOG_DIR = os.getenv("LOG_DIR", "logs")
 SEND_INTERVAL_MIN = os.getenv("SEND_INTERVAL_MIN")
 USE_AD = os.getenv("USE_AD")
 LOCAL_SERVER = os.getenv("LOCAL_SERVER")
